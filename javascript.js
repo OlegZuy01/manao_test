c0f254d758db548f10fcd537e530950be2194463
OLEG <oleg.zuev.01@gmail.com>
2019-10-02 01:00:51 +0300

    add

---
+++ javascript.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    var clickButtonForm=$(".clickButtonForm");
+        clickButtonForm.click(function(){ //функция , которая работает после срабатывания кнопка
+            var inputLoginValue = $("#login");
+            var inputPasswordValue = $("#password");
+            var inputConfirmPasswordValue = $("#confirmPassword"); //помещаем в переменные
+            var inputEmailValue = $("#email");
+            var inputNameValue = $("#name");
+            if(inputLoginValue.val() == "" || inputPasswordValue.val() == "" || inputConfirmPasswordValue.val() == "" || inputEmailValue.val() == "" || inputNameValue.val() == ""){
+                $("#error").text("Вы не заполненли все поля").renoveClass("success").addClass("error").show().delay(5000).fadeOut(500);
+            }
+            else{
+                $.ajax({
+                    url: "registration.php",
+                    type: "POST",
+                    data: {login: inputLoginValue.val(),password: inputPasswordValue.val(),confirmPassword: inputConfirmPasswordValue.val(),email: inputEmailValue.val(),name: inputNameValue.val()}
+
+                    success: function(data) {
+                        if(data == 0) {
+                            $("#error").text("вы успешно зарегистрировались").renaveClass("error").addClass("success").show().delay(5000).fadeOut(500);
+                        }
+                        if(data == email_busy){
+                            $("#error").text("Пользователь с таким email-ом уже зарегистрирован(").renoveClass("success").addClass("error").show().delay(5000).fadeOut(500);
+                        }
+                        if(data == login_busy){
+                            $("#error").text("Пользователь с таким login-ом уже зарегистрирован(").renoveClass("success").addClass("error").show().delay(5000).fadeOut(500);
+                        }
+                        if(data == passwords_do_not_match){
+                            $("#error").text("Пароли не совпадают .").renoveClass("success").addClass("error").show().delay(5000).fadeOut(500);
+                        }
+                    }
+                })
+            }
+        });
+    });
--- registration.php
+++ registration.php
@@ -1,41 +1,46 @@
 <?php
-session_start();
-if(isset($_POST['login'])&&isset($_POST['password'])&&isset($_POST['confirm_password'])&&isset($_POST['email'])&&isset($_POST['name'])){
-    $login=$_POST['login'];
-    $password=$_POST['password'];
-    $confirm_password=$_POST['confirm_password'];
-    $email=$_POST['email'];
-    $name=$_POST['name'];
-    if($password!=$confirm_password){
-        echo "Пароли не совпадают .";
-    }
-    else{
-        $xml= simplexml_load_file('db.xml');
-        $i=0;
-        foreach($xml->user as $user){
-            if($user->email==$email){
-                $i++;
-                echo "Пользователь с таким email-ом уже зарегистрирован( ";
-            }
-            if($user->login==$login){
-                echo "Пользователь с таким login-ом уже зарегистрирован( ";
-                $i++;
-            }
-        }
-        if($i==0){
-            $password_hash = password_hash($password,PASSWORD_DEFAULT);
-            $new_user=$xml->addChild('user');
-            $new_user->addChild('login','$login');
-            $new_user->addChild('password_hash','$$password_hash');
-            $new_user->addChild('email','$email');
-            $new_user->addChild('name','$name');
-            $xml->asXml('db.xml');
-            echo "привет ".$name;
-        }
-
-    }
+if(isset($_POST['login'])) {
+    $login = $_POST['login'];
+}
+if(isset($_POST['password'])) {
+    $password = $_POST['password'];
+}
+if(isset($_POST['confirm_password'])) {
+    $confirm_password = $_POST['confirm_password'];
+}
+if(isset($_POST['email'])) {
+    $email = $_POST['email'];
+}
+if(isset($_POST['name'])) {
+    $name = $_POST['name'];
+}
+if($password!=$confirm_password){
+    echo "passwords_do_not_match";
 }
 else{
-    echo "Вы ввели не все данные !";
+    $xml= simplexml_load_file('db.xml');
+    $i=0;
+    foreach($xml->user as $user){
+        if($user->email==$email){
+            $i++;
+            echo "email_busy";
+        }
+        if($user->login==$login){
+            echo "login_busy";
+            $i++;
+        }
+    }
+    if($i==0){
+        $password_hash = password_hash($password,PASSWORD_DEFAULT);
+        $new_user=$xml->addChild('user');
+        $new_user->addChild('login',$login);
+        $new_user->addChild('password_hash',$password_hash);
+        $new_user->addChild('email',$email);
+        $new_user->addChild('name',$name);
+        $xml->asXml('db.xml');
+        echo 0;
+    }
 }
+
+
 ?>
