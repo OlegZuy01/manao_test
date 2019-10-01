400ab59bf4073854483a27d075ccb293b339569d
OLEG <oleg.zuev.01@gmail.com>
2019-10-01 19:42:03 +0300

    add file

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$xmlstr=<<<XML
+    <users>
+        <user>
+            <login>Olezha</login>
+            <password_hash>$2y$10\$zLg/kxPr6Mx6XWUge.SdiuHeskV97cDWPyg04MT76PRBrZgJhPEtu</password_hash>
+            <email>oleg.zuev.01@gmail.com</email>
+            <name>Oleg</name>
+        </user>
+        
+    </users>
+XML;
+
+
+?>
---
+++ registration.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+include 'db.php';
+if(isset($_POST['login'])&&isset($_POST['password'])&&isset($_POST['confirm_password'])&&isset($_POST['email'])&&isset($_POST['name'])){
+    $login=$_POST['login'];
+    $password=$_POST['password'];
+    $confirm_password=$_POST['confirm_password'];
+    $email=$_POST['email'];
+    $name=$_POST['name'];
+    if($password!=$confirm_password){
+        echo "Пароли не совпадают .";
+    }
+    else{
+        $xml= new SimpleXMLElement($xmlstr);
+        $i=0;
+        foreach($xml->user as $user){
+            if($user->email==$email){
+                $i++;
+                echo "Пользователь с таким email-ом уже зарегистрирован( ";
+            }
+            if($user->login==$login){
+                echo "Пользователь с таким login-ом уже зарегистрирован( ";
+                $i++;
+            }
+        }
+        if($i==0){
+            $password_hash = password_hash($password,PASSWORD_DEFAULT);
+            $new_user=$xml->addChild('user');
+            $new_user->addChild('login','$login');
+            $new_user->addChild('password_hash','$$password_hash');
+            $new_user->addChild('email','$email');
+            $new_user->addChild('name','$name');
+
+        }
+
+    }
+}
+else{
+    echo "Вы ввели не все данные !";
+}
+?>
